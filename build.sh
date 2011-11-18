6f41b5418820075734710b5bc5be5a6f65eb7059
Christian Weiske <cweiske@cweiske.de>
2011-11-18 18:27:51 +0100

    add translation template to ipkg

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ for i in po/??.mo; do
     mkdir -p tmp/$extdir/locale/$lang/LC_MESSAGES
     cp "$i" tmp/$extdir/locale/$lang/LC_MESSAGES/CurlyTx.mo
 done
+cp po/messages.pot tmp/$extdir/locale/CurlyTx.mo
 
 python -O -m compileall src/ -f
 cp src/*.py tmp/$extdir/
